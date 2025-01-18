82630ec80f6ea57b32df7d505ff860d4f19ed508
Артем Белов <theflash.0320@gmail.com>
2025-01-18 15:07:44 +0500

    fix wb price getting

--- getWbPrice.js
+++ getWbPrice.js
@@ -2,13 +2,19 @@ import { chromium } from "playwright";
 
 export default async function getPrice() {
   console.log("Начинаю поиск цены на Wildberries");
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
+  const browser = await chromium.launch({ headless: true });
+  const context = await browser.newContext({
+    userAgent:
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+  });
   const page = await context.newPage();
 
   await page.goto(
     "https://www.wildberries.ru/catalog/3518597/detail.aspx?targetUrl=MS&size=13525664"
   );
+  await page.mouse.move(100, 100);
+  await page.waitForTimeout(1000);
+  await page.mouse.move(500, 400);
   await page.waitForSelector(".price-block__final-price.wallet");
   const textContent = await page.$eval(
     ".price-block__final-price.wallet",
